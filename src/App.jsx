31eacf7f641b62d03b551e2936a8a9bfb4c24e0c
Damian@Vu <minhvdq@gmail.com>
2025-02-09 20:19:01 -0500

    setup2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from './components/Home/HomePage'
+import HomePage from './components/Home/HomePage'
 import BoardPage from './components/Board/BoardPage'
 import EventsPage from './components/Events/EventsPage'
 import ProjectsPage from './components/Home/ProjectPage'
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/events' element={<EventsPage />} />
-        <Route path='/boards' element={<BoardsPage />} />
+        <Route path='/boards' element={<BoardPage />} />
         <Route path='/projects' element={<ProjectsPage />} />
       </Routes>
     </BrowserRouter>
